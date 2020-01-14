8781a2b8bed90a824d8f1d99216b0c82cc1f7832
Matt Sievers <msievers@thegeneral.com>
2020-01-14 13:18:07 -0600

    Lifecycle app github URL txt file

---
+++ app/src/main/java/com/example/lifecycleapp/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.lifecycleapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_clicker;
+    TextView tv_counter;
+    int clicks = 0;
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("clickerValue", clicks);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        clicks = savedInstanceState.getInt("clickerValue");
+        tv_counter = findViewById(R.id.tv_counter);
+        tv_counter.setText(Integer.toString(clicks));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecyclefilter", "the app is started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecyclefilter", "the app is stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecyclefilter", "the app is destroyed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecyclefilter", "the app is paused");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecyclefilter", "the app is resumed");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecyclefilter", "the app is restarted");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("lifecyclefilter", "the app is created");
+
+        btn_clicker = findViewById(R.id.btn_clicker);
+        tv_counter = findViewById(R.id.tv_counter);
+
+        btn_clicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicks++;
+                tv_counter.setText(Integer.toString(clicks));
+            }
+        });
+    }
+}
